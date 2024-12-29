90a99817e285952c51440c36461a3f7e000ec80d
Asko Nõmm <asko@nmm.ee>
2024-12-29 22:15:17 +0200

    bump

--- dompa/dompa_test.py
+++ dompa/dompa_test.py
@@ -13,3 +13,17 @@ def test_html_equality2():
     dom = Dompa(html)
 
     assert dom.html() == html
+
+
+def test_html_equality3():
+    html = '<div class=\"test test2 test3\">Hello</div>'
+    dom = Dompa(html)
+
+    assert dom.html() == html
+
+
+def test_html_equality4():
+    html = '<input type=\"radio\" checked>'
+    dom = Dompa(html)
+
+    assert dom.html() == html
